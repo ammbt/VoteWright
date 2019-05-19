aebd3aa846269eb651604748447e11412bdbf21b
Austin McDaniel <admcdaniel533@gmail.com>
2019-05-19 12:32:10 -0500

    Migrated ion components. App runs, still needs authentication to be reworked (authguard).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { VoteWright } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginPage } from './pages/login/login';
-import { PlayersPage } from './pages/players/players';
-import { GroupPage } from './pages/group/group';
 import { Authentication } from './services/authentication';
 import { AppStorage } from './services/app-storage';
 
 @NgModule({
 	declarations: [
-		VoteWright,
-		LoginPage,
-		PlayersPage,
-		GroupPage],
+		VoteWright],
 	entryComponents: [],
 	imports: [
 		BrowserModule,
 		IonicModule.forRoot(),
 		AppRoutingModule],
-	// providers: [
-	// 	Authentication,
-	// 	StatusBar,
-	// 	SplashScreen,
-	// 	AppStorage,
-	// 	{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-	// ],
+	providers: [
+		Authentication,
+		StatusBar,
+		SplashScreen,
+		AppStorage,
+		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+	],
 	bootstrap: [VoteWright]
 	})
 	export class AppModule {}
